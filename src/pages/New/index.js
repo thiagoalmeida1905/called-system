840b0e18a5a6d6b8209431f83597f8e3ddc6b474
thiagoalmeida1905 <thi.almeida1905@gmail.com>
2024-01-23 12:25:39 -0300

    feat: implementei a função de cadastrar um chamado no bd

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,19 +1,95 @@
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 import './new.css';
 import { FiPlusCircle } from 'react-icons/fi';
+import { AuthContext } from '../../contexts/auth';
+import { db } from '../../services/firebaseConnection';
+import { collection, getDocs, getDoc, doc, addDoc } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
+
+const listRef = collection(db, 'customers');//REFERÊNCIA À COLEÇÃO DE CLIENTE
 
 export default function New() {
+    const { user } = useContext(AuthContext);
 
-    const [ customers, setCustomers ] = useState([])
+    const [ customers, setCustomer ] = useState([]);
+    const [ loadCustomer, setLoadCustomer ] = useState(true);
+    const [ customerSelected, setCustomerSelected ] = useState(0)
     const [ complemento, setComplemento ] = useState('');
-    const [ suporte, setAssunto ] = useState('Suporte');
+    const [ assunto, setAssunto ] = useState('Suporte');
     const [ status, setStatus ] = useState('Aberto');
+
+    useEffect( ()=>{ //BUSCANDO INFORMAÇÃO DE TODOS OS CLIENTES CADASTRADOS NA MONTAGEM DO COMPONENTE
+        async function loadCustomer(){
+            const querySnapshot = await getDocs(listRef) //retorna o que tem na coleção de clientes
+            .then ( (snapshot) => { //snapshot vai conter os dados da coleção de clientes
+                let lista = [];
+
+                snapshot.forEach(( doc ) => { //Percorrer os dados trazidos pela promise(snapshot)
+                    lista.push({ // colocar os dados na lista vazia
+                        id: doc.id,
+                        nomeFantasia: doc.data().nomeFantasia
+                    })
+                })
+
+                if (snapshot.docs.size === 0){ //se não tiver nenhum cliente
+                    setCustomer([{id: '1', nomeFantasia: 'FREELA'}])
+                    setLoadCustomer(false)
+                    return;
+                }
+
+                setCustomer(lista);
+                setLoadCustomer(false);
+            })
+            .catch((error) => {
+                console.log('ERRO AO BUSCAR OS CLIENTES', error)
+                setLoadCustomer(false)
+                setCustomer([{id: '1', nomeFantasia: 'FREELA'}])
+
+            })
+        }
+
+        loadCustomer()
+    }, [])
 
     function handleOptionChange(e) {
         setStatus(e.target.value)
+    }
+
+    function handleChangeSelect(e) {
+        setAssunto(e.target.value)
+    }
+
+    function handleChangeCustomer(e) {
+        setCustomerSelected(e.target.value)
+    }
+
+
+    //----------------- Função para cadastar usuario ------------------------------------ //
+
+    async function handleRegister (e) {
+        e.preventDefault();
+
+        await addDoc(collection(db, 'chamados'), {
+            created: new Date(),
+            cliente: customers[customerSelected].nomeFantasia,
+            clienteId: customers[customerSelected].id,
+            assunto: assunto,
+            complemento: complemento,
+            status: status,
+            userId: user.uid
+        })
+        .then ( () => {
+            toast.success('Chamado registrado');
+            setComplemento('')
+            setCustomerSelected(0)
+        })
+        .catch( (error) => {
+            toast.error('Ops! erro ao registrar, tente novamente mais tarde!')
+        })
+        
     }
 
 
@@ -27,16 +103,27 @@ export default function New() {
                 </Title>
 
                 <div className="container">
-                    <form className='form-profile'>
+                    <form className='form-profile' onSubmit={handleRegister}>
 
                         <label>Clientes</label>
-                        <select>
-                            <option key={1} value={1}>Mercado teste</option>
-                            <option key={2} value={2}>Loja teste</option>
-                        </select>
+                        {
+                            loadCustomer ? (
+                                <input type='text' disabled={true} value={'Carregando...'}/>
+                            ) : (
+                                <select value={customerSelected} onChange={handleChangeCustomer}>
+                                    { customers.map( (item, index) => {
+                                        return (
+                                            <option key={index} value={index}>
+                                                {item.nomeFantasia}
+                                            </option>
+                                        )
+                                    }) }
+                                </select>
+                            )
+                        }
 
                         <label>Assunto</label>
-                        <select>
+                        <select value={assunto} onChange={handleChangeSelect}>
                             <option value="Suporte">Suporte</option>
                             <option value="Visita Tecnica">Visita Tecnica</option>
                             <option value="Financeiro">Financeiro</option>
